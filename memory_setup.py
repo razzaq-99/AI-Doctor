081bd3754ff44ab82c42c4f90b483a991df97284
abdul Razzaq <ar8937562@gmail.com>
2025-08-18 18:46:57 +0500

    data divided into chunks

--- memory_setup.py
+++ memory_setup.py
@@ -1,6 +1,8 @@
 from langchain_community.document_loaders import PyPDFLoader , DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
 
+load_dotenv()
 
 data_path = "data/"
 
@@ -9,6 +11,15 @@ def load_pdf(data):
     
     documents = loader.load()
     return documents
-
 documents = load_pdf(data_path)
 print("length : ", len(documents))
+
+
+
+def create_chunks(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    chunks = text_splitter.split_documents(extracted_data)
+    return chunks
+
+text_chunks = create_chunks(documents)
+print('length :', len(text_chunks))
